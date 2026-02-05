534e1ced01b443b54a294e06f2b8129ee3d15e93
BakhDev <bakhteegames@gmail.com>
2026-02-06 00:53:29 +0300

    feat: Unit_Volt

---
+++ Assets/Scripts/Units/Unit_Volt.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+namespace AetherRenaissance{
+public class Unit_Volt:MonoBehaviour{
+public int hp=80,cost=100,damage=15;
+public float range=8f,speed=4.5f;
+void Start(){Debug.Log("[Unit_Volt] Ranged unit ready!");}
+}
+}
