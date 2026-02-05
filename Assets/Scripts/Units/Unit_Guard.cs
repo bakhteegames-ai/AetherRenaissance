7f05de8145bb6c5165e73d0776e985941cda7a5e
BakhDev <bakhteegames@gmail.com>
2026-02-06 00:54:22 +0300

    feat: Unit_Guard + Barracks + SelectionManager final batch

---
+++ Assets/Scripts/Units/Unit_Guard.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+namespace AetherRenaissance{
+public class Unit_Guard:MonoBehaviour{
+public int hp=200,cost=150,damage=25;
+public float range=1.5f,speed=3f;
+void Start(){Debug.Log("[Unit_Guard] Tank unit ready!");}
+}
+}
